d43db7e659f2d203ca2f757233e895a1862fc262
Jeffrey Lan <jtlan.sf@gmail.com>
2020-01-07 11:58:48 -0500

    discrete functions written in test.js

--- test.js
+++ test.js
@@ -9,40 +9,91 @@ divArtist.innerHTML = `by Prince feat. ${renderArtist}`;
 divLyrics.innerHTML = renderLyrics;
 
 /* ----- REDUCE pifyLineArr ----- */
-let renderLyrics = pifyLineArr.reduce((a, b) => `${a}<br>${b}`);
-// genericized
+// reduce function
 const lyricReduce = arr => {
-  arr.reduce((a, b) => `${a}<br>${b}`);
+  return arr.reduce((a, b) => `${a}<br>${b}`);
 };
-renderLyrics = lyricReduce(pifyLineArr);
+// invocation
+const renderLyrics = lyricReduce(pifyLineArr);
 
 /* ----- MUTATE origLineArr TO pifyLineArr ----- */
+// empty array to hold mutated lyrics
 const pifyLineArr = [];
-
-const mutateLine = lyrics => {
-  for (jkl = 0; jkl < lyrics.length; ++jkl) {
-    const mutate = lyrics[jkl].replace(
-      /\bi\b|\byou\b|\byour\b|\bare\b|\bto\b|\btoo\b|\bfor\b|\bbe\b|\bknow\b|\bI\b|\bYou\b|\bYour\b|\bAre\b|\bTo\b|\bToo\b|\bFor\b|\bBe\b|\bKnow\b/gi,
-      matched => {
-        return pifyBasic[matched];
-      }
-    );
+// mutation function
+const mutateLine = (lyricsArr, keyObject) => {
+  const RE = new RegExp(
+    `\\b${Object.keys(keyObject).join("\\b|\\b")}\\b`,
+    "gi"
+  );
+  for (jkl = 0; jkl < lyricsArr.length; ++jkl) {
+    const mutate = lyricsArr[jkl].replace(RE, matched => {
+      return keyObject[matched];
+    });
     pifyLineArr.push(mutate);
   }
 };
-mutateLine(lines);
-// genericized
-const mutateKeys = selectedRegExp => {
-  const keyArr = [];
-  // format keys for RegExp
-  for (jkl = 0; jkl < selectedRegExp.length; ++jkl) {
-    keyArr.push(`\b${selectedRegExp.keys[jkl]}\b|`);
+// invocation
+mutateLine(origLineArr, pifyBasic);
+
+/* ----- SPLIT origLineStr TO origLineArr ----- */
+const origLineArr = origLineStr.splig("\n");
+
+/* ----- CREATE origLineStr VARIABLE ----- */
+// shorthand for Musixmatch lyrics object, lyrics text, and copyright notice
+const mxmLyrObj = res.data.message.body.lyrics;
+let lyricsFound = mxmLyrObj.lyrics_body;
+let lyricsCopyright = mxmLyrObj.lyrics_copyright;
+// append copyright to lyrics
+let origLineStr = `${lyricsFound}\n\n${lyricsCopyright}`;
+
+/* ----- GET LYRICS ----- */
+const res = await axios.get(
+  `${apiURL}matcher.lyrics.get?q_track=${inputTitle}&q_artist=${inputArtist}&apikey=${apiKey}`
+);
+
+/* ----- GET SONG TITLE ----- */
+const resTitle = await axios.get(
+  `${apiURL}track.search?q_track=${inputTitle}&q_artist=${inputArtist}&page_size=1&apikey=${apiKey}`
+);
+let mxmTitle = resTitle.data.message.body.track_list[0].track.track_name;
+
+/* ----- TRANSFORM mxmTitle ----- */
+const removeExtraNames = originalTitle => {
+  // add transforms to remove anything in () or []
+};
+let renderTitle = removeExtraNames(mxmTitle);
+
+/* ----- GET ARTIST NAME ----- */
+const resArtist = await axios.get(
+  `${apiURL}artist.search?q_artist=${inputArtist}&page_size=1&apikey=${apiKey}`
+);
+let renderArtist =
+  resArtist.data.message.body.artist_list[0].artist.artist_name;
+
+/* ----- API CALL ----- */
+const getData = async () => {
+  try {
+    // get artist name
+    // get song title
+    // get lyrics
+  } catch {
+    // remove this error message at next search
+    document.querySelector(".error").innerHTML = "👁️ couldn't find Ur song";
+    console.error("something went wrong");
   }
-  // concatenate keys
-  const intermediateStr = keyArr.reduce((a, b) => a + b);
-  // remove last pipe [from (https://tecadmin.net/remove-last-character-from-string-in-javascript/)]
-  const innerStr = intermediateStr.substring(0, str.length - 1);
-  return `/${innerStr}/gi`;
+  getData();
 };
 
-const mutateLine = (lyrics, selectedRegExp) => {};
+/* ----- FORMAT INPUT FOR API ----- */
+let inputTitle = document.querySelector("#title").value;
+let inputArtist = document.querySelector("#artist").value;
+inputTitle = inputTitle.replace(" ", "+");
+inputArtist = inputArtist.replace(" ", "+");
+
+/* ----- GET FORM DATA ----- */
+function getFormData(e) {
+  e.preventDefault();
+  // get data
+}
+const form = document.querySelector("form");
+form.addEventListener("submit", getFormData);
--- test2.js
+++ test2.js
@@ -1,6 +1,6 @@
 const pifyBasic = {
   // Object.keys = original words; Object.values = Princified substitutions
-  i: "👁️",
+  i: "👁️ ",
   you: "U",
   your: "Ur",
   are: "R",
@@ -9,7 +9,7 @@ const pifyBasic = {
   for: "4",
   be: "B",
   know: "no",
-  I: "👁️",
+  I: "👁️ ",
   You: "U",
   Your: "Ur",
   Are: "R",
@@ -20,18 +20,71 @@ const pifyBasic = {
   Know: "No"
 };
 
-const holyTerrain =
-  "Will you still be there for me\nOnce I'm yours to obtain?\nOnce my fruits are for taking\nAnd you flow through my veins?\nDo you still think I'm beautiful\nWhen my tears fall like rain?\nMy love is so bountiful\nFor a man, who is true to me";
+const juice = [
+  "Mirror, mirror on the wall",
+  "Don't say it, 'cause I know I'm cute (Oh, baby)",
+  "Louis down to my drawers",
+  "LV all on my shoes (Ooh, baby)",
+  "",
+  "I be dripping so much sauce",
+  "Gotta been looking like ragù (Ooh, baby)",
+  "Lit up like a crystal ball",
+  "That's cool, baby, so is you",
+  "That's how I roll",
+  "",
+  "If I'm shining, everybody gonna shine",
+  "(Yeah, I'm goals)",
+  "I was born like this, don't even gotta try",
+  "(Now you know)",
+  "I'm like Chardonnay, get better over time",
+  "(So you know)",
+  "Heard you say I'm not the baddest bitch, you lie",
+  "",
+  "Ain't my fault that I'm out here getting loose",
+  "Gotta blame it on the Goose",
+  "Gotta blame it on my juice, baby",
+  "Ain't my fault that I'm out here making news",
+  "I'm the pudding in the proof",
+  "Gotta blame it on my juice",
+  "",
+  "Ya-ya-ee, ya-ya-ee, ya-ya-ee, ya-ya-ee",
+  "...",
+  "",
+  "******* This Lyrics is NOT for Commercial use *******",
+  "(1409618871795)",
+  "",
+  "Lyrics powered by www.musixmatch.com. This Lyrics is NOT for Commercial use and only 30% of the lyrics are returned."
+];
 
-const mutateKeys = (selectedObject, inputText) => {
+// const makeRegExp = selectedObject => {
+//   const RE = new RegExp(
+//     `\\b${Object.keys(selectedObject).join("\\b|\\b")}\\b`,
+//     "gi"
+//   );
+//   console.log(RE);
+// };
+
+// mutateKeys(pifyBasic);
+const newArr = [];
+
+const mutateLine = (lyrics, keyObject) => {
   const RE = new RegExp(
-    `\\b${Object.keys(selectedObject).join("\\b|\\b")}\\b`,
+    `\\b${Object.keys(keyObject).join("\\b|\\b")}\\b`,
     "gi"
   );
-  console.log(RE);
-  inputText.replace(RE, matched => {
-    return selectedObject[matched];
-  });
+  for (jkl = 0; jkl < lyrics.length; ++jkl) {
+    const mutate = lyrics[jkl].replace(RE, matched => {
+      return keyObject[matched];
+    });
+    newArr.push(mutate);
+    console.log(newArr);
+  }
 };
+mutateLine(juice, pifyBasic);
 
-mutateKeys(pifyBasic, holyTerrain);
+const lyricReduce = arr => {
+  return arr.reduce((a, b) => `${a}<br>${b}`);
+};
+// invocation
+const renderLyrics = lyricReduce(newArr);
+console.log(renderLyrics);
